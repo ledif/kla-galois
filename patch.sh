980fcb652aaa7c065d896bcdafd210a8cb2ad936
Adam Fidel <fidel@cse.tamu.edu>
2014-08-14 10:54:28 -0500

    Fixing script.

--- patch.sh
+++ patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-http://iss.ices.utexas.edu/projects/galois/downloads/Galois-2.1.8.tar.gz
+wget http://iss.ices.utexas.edu/projects/galois/downloads/Galois-2.1.8.tar.gz
 
 tar -xzvf Galois-2.1.8.tar.gz
 patch -p0 < kla.patch
